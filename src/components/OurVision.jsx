bb98f73c20b6457bad7a6b1462b30647c29ffad7
Codinglone <codinglone@gmail.com>
2023-10-04 00:56:20 +0200

    Finalized OurVision

--- src/components/OurVision.jsx
+++ src/components/OurVision.jsx
@@ -26,31 +26,31 @@ const OurVision = () => {
           background or resources, has a clear pathway to professional success.
           We believe that by simulating real-world job scenarios, we are giving
           students and recent graduates a unique platform to hone crucial skills
-          that are often overlooked in conventional academic settings. 
-          <br /><br />
-          Our
-          mission extends beyond skill-building. We are also committed to
+          that are often overlooked in conventional academic settings.
+          <br />
+          <br />
+          Our mission extends beyond skill-building. We are also committed to
           forging strong industry partnerships, ensuring that the training we
           offer aligns with the evolving demands of the global job market. With
           each simulation, we hope to create a dynamic and inclusive space where
           individuals can experiment, learn, and grow without fear of failure,
           and where their achievements are celebrated and translated into
           tangible career opportunities.
-          <br /><br />
-           By bridging the skills gap, BAG aims to
-          catalyze socio-economic growth in Rwanda, empowering the next
-          generation of professionals to drive innovation, foster community
-          development, and make impactful contributions to the global workforce.
-          Our multifaceted approach encompasses not only skill development but
-          also a commitment to inclusivity, innovation, and the advancement of
-          individuals as they embark on their journeys toward professional
-          success.
+          <br />
+          <br />
+          By bridging the skills gap, BAG aims to catalyze socio-economic growth
+          in Rwanda, empowering the next generation of professionals to drive
+          innovation, foster community development, and make impactful
+          contributions to the global workforce. Our multifaceted approach
+          encompasses not only skill development but also a commitment to
+          inclusivity, innovation, and the advancement of individuals as they
+          embark on their journeys toward professional success.
         </p>
         <div className="flex py-8 justify-center">
-        <button className="rounded-md border-2 border-white px-12 text-white py-2 hover:border-[#000629] hover:bg-blue-700 font-bold">
-          Get In Touch
-        </button>
-      </div>
+          <button className="rounded-md border-2 border-white px-12 text-white py-2 hover:border-[#000629] hover:bg-blue-700 font-bold">
+            Get In Touch
+          </button>
+        </div>
       </div>
     </section>
   );
